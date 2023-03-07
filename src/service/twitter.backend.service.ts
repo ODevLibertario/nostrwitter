bfc90af21a599a3b77e4cf5ff4bf0c10a5b860a3
ODevLibertario <odevlibertario@proton.me>
2023-03-07 20:14:49 -0300

    fix

--- src/service/twitter.backend.service.ts
+++ src/service/twitter.backend.service.ts
@@ -5,7 +5,7 @@ import {toastFailure, toastSuccess} from "../toast.utils";
 export class TwitterBackendService {
 
     public getAuthorization(){
-        return fetch(process.env.TWITTER_BACKEND_HOST +"/twitter/auth").then(response => {
+        return fetch(process.env.REACT_APP_TWITTER_BACKEND_HOST +"/twitter/auth").then(response => {
             return response.json().then(json => json)
         }).catch(e => {
             toast("Authorization to Twitter failed: " + e, toastFailure)
@@ -13,7 +13,7 @@ export class TwitterBackendService {
     }
 
     tweet(pin: string, authorization: {oauth_token: string, oauth_token_secret: string}, post: string){
-        return fetch(process.env.TWITTER_BACKEND_HOST +"/twitter/tweet", {
+        return fetch(process.env.REACT_APP_TWITTER_BACKEND_HOST +"/twitter/tweet", {
             method: 'POST',
             body: JSON.stringify({pin, oauth_token: authorization.oauth_token, oauth_token_secret: authorization.oauth_token_secret, post})
         }).then(response => {
