7fef99684e7b976241ae8097ca42722c357d4030
ODevLibertario <odevlibertario@proton.me>
2023-03-14 14:24:31 -0300

    fix

--- src/components/TwitterCallback.tsx
+++ src/components/TwitterCallback.tsx
@@ -1,10 +1,12 @@
 import {useLocation} from "react-router-dom";
 
-let query = useQuery();
 
-function useQuery() {
-    return new URLSearchParams(useLocation().search);
-}
 export function TwitterCallback(props: any) {
+    function useQuery() {
+        return new URLSearchParams(useLocation().search);
+    }
+
+    let query = useQuery();
+
     return <div>Please wait... {query.get("oauth_token")} and {query.get("oauth_verifier")} </div>;
 }
