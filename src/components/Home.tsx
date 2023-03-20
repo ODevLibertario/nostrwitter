e2f196a8dfdc16c357e352c10044abd6930aff99
odevlibertario <odevlibertario.proton.me>
2023-03-20 00:33:35 -0300

    fix

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -163,8 +163,8 @@ class Home extends React.Component<any, any> {
                         </Button>
                     </Form>}
                     {this.state.loggedInNostr && <div>
-                        {!this.extendedWindow.nostr && <span style={{color: '#8e2ebe', fontWeight: 'bold', fontSize: '20px'}}>Logged in to Nostr with: nsec*****{this.state.nostrNsec.substring(this.state.nostrNsec.length - 5)}</span>}
-                        {this.extendedWindow.nostr && <span style={{color: '#8e2ebe', fontWeight: 'bold', fontSize: '20px'}}>Logged in to Nostr with a browser extension, please check if your private key is setup correctly there.</span>}
+                        {this.extendedWindow && !this.extendedWindow.nostr && <span style={{color: '#8e2ebe', fontWeight: 'bold', fontSize: '20px'}}>Logged in to Nostr with: nsec*****{this.state.nostrNsec.substring(this.state.nostrNsec.length - 5)}</span>}
+                        {this.extendedWindow && this.extendedWindow.nostr && <span style={{color: '#8e2ebe', fontWeight: 'bold', fontSize: '20px'}}>Logged in to Nostr with a browser extension, please check if your private key is setup correctly there.</span>}
                         <Button variant="primary"
                                 style={{backgroundColor: '#8e2ebe', fontWeight: 'bold', float: 'right', marginTop: '3%'}}
                                 onClick={this.logoutNostr.bind(this)}>
